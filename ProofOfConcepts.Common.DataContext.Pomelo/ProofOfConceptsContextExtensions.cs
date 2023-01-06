a5686e353fb86b3644f939fc72fc22fcf3b61007
Amit Ganguli <amit@kaikanutrition.in>
2023-01-06 18:23:49 +0530

    Initial Commit for Pomelo data context

---
+++ ProofOfConcepts.Common.DataContext.Pomelo/ProofOfConceptsContextExtensions.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore; // Use MySql
+using Microsoft.Extensions.DependencyInjection; // IServiceCollection
+using Microsoft.Extensions.Logging; // LogLevel
+
+namespace ProofOfConcept.Shared;
+public static class SakilaContextContextExtensions
+{
+    /// <summary>
+    /// Adds NorthwindContext to the specified IServiceCollection. Uses the MySql database provider.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="connectionString">Set to override the default.</param>
+    /// <returns>An IServiceCollection that can be used to add more services.</returns>
+    public static IServiceCollection AddSakilaContext(this IServiceCollection services,
+     string connectionString = "server=localhost;user=root;password=Gold$gym45;database=sakila")
+    {
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+
+        services.AddDbContext<SakilaContext>(dbContextOptions => dbContextOptions
+                .UseMySql(connectionString, serverVersion)
+                // The following three options help with debugging, but should be changed or removed for production.
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors()
+        );
+        return services;
+    }
+}
--- ProofOfConcepts.Common.DataContext.Pomelo/SakilaContext.cs
+++ ProofOfConcepts.Common.DataContext.Pomelo/SakilaContext.cs
@@ -62,7 +62,7 @@ public partial class SakilaContext : DbContext
     public virtual DbSet<Store> Stores { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseMySql("server=localhost;user=root;password=Gold$gym45;database=sakila", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
