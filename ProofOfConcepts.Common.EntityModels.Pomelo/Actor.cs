705dddc969173d3b59c111da9dcba97cc1483e0c
Amit Ganguli <amit@kaikanutrition.in>
2023-01-06 17:22:12 +0530

    Initial commit on success creation of scaffolding

---
+++ ProofOfConcepts.Common.EntityModels.Pomelo/Actor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProofOfConcept.Shared;
+
+[Table("actor")]
+[Index("LastName", Name = "idx_actor_last_name")]
+public partial class Actor
+{
+    [Key]
+    [Column("actor_id")]
+    public ushort ActorId { get; set; }
+
+    [Column("first_name")]
+    [StringLength(45)]
+    public string FirstName { get; set; } = null!;
+
+    [Column("last_name")]
+    [StringLength(45)]
+    public string LastName { get; set; } = null!;
+
+    [Column("last_update", TypeName = "timestamp")]
+    public DateTime LastUpdate { get; set; }
+
+    [InverseProperty("Actor")]
+    public virtual ICollection<FilmActor> FilmActors { get; } = new List<FilmActor>();
+}
